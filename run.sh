167fbfc066e7e714cd2b3c6f1079c612c96ed919
oushesh <oushesh@gmail.com>
2021-01-29 20:44:59 +0100

    added run.sh but need to fix pangolin installation

--- run.sh
+++ run.sh
@@ -2,4 +2,13 @@
 #and executed automatically
 
 #Pangolin installation
-git clone https://github.com/uoip/pangolin.git
+echo 'Starting installation on Windows'
+#git clone https://github.com/uoip/pangolin.git
+cd pangolin
+mkdir build
+cd build
+cmake ..
+make -j8
+cd ..
+python setup.py install
+echo 'Done!'
