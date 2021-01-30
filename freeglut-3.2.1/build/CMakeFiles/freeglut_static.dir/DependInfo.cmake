d077f2633be031aec64c81388bb284b3c3d09e4c
oushesh <oushesh@gmail.com>
2021-01-30 15:24:22 +0100

    installed OpenGL, glew & test_OpenGL.cpp

---
+++ freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/DependInfo.cmake
@@ -0,0 +1,89 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_callbacks.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_callbacks.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_cursor.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_cursor.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_display.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_display.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_ext.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_ext.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_font.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_font.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_font_data.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_font_data.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_gamemode.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_gamemode.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_geometry.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_geometry.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_gl2.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_gl2.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_init.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_init.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_input_devices.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_input_devices.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_joystick.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_joystick.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_main.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_main.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_menu.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_menu.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_misc.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_misc.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_overlay.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_overlay.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_spaceball.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_spaceball.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_state.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_state.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_stroke_mono_roman.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_stroke_mono_roman.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_stroke_roman.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_stroke_roman.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_structure.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_structure.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_teapot.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_teapot.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_videoresize.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_videoresize.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/fg_window.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/fg_window.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_cursor_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_cursor_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_display_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_display_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_ext_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_ext_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_gamemode_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_gamemode_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_init_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_init_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_input_devices_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_input_devices_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_joystick_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_joystick_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_main_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_main_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_menu_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_menu_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_spaceball_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_spaceball_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_state_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_state_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_structure_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_structure_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/mswin/fg_window_mswin.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/mswin/fg_window_mswin.c.obj"
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/src/util/xparsegeometry_repl.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/src/util/xparsegeometry_repl.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "FREEGLUT_STATIC"
+  "HAVE_CONFIG_H"
+  "WINVER=0x0601"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_WIN32_WINNT=0x0601"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../include"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/freeglut.rc" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/freeglut.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "FREEGLUT_STATIC"
+  "HAVE_CONFIG_H"
+  "WINVER=0x0601"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_WIN32_WINNT=0x0601"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "../src"
+  "../include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ freeglut-3.2.1/build/CMakeFiles/timer_callback_static.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/progs/demos/timer_callback/timer.c" "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/timer_callback_static.dir/progs/demos/timer_callback/timer.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "FREEGLUT_STATIC"
+  "HAVE_CONFIG_H"
+  "WINVER=0x0601"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_WIN32_WINNT=0x0601"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Oushesh/Documents/WORKSPACE/toySLAM/freeglut-3.2.1/build/CMakeFiles/freeglut_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
